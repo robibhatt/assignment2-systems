20436f8fdb88d2698dca92d644b5ce6f3a19175a
Robi Bhattacharjee <luj210@galvani-login.sdn>
2025-12-09 23:35:59 +0100

    implementing flash attention in pure pytorch

--- cs336_systems/triton_speed_test.py
+++ cs336_systems/triton_speed_test.py
@@ -4,26 +4,33 @@ import triton
 from cs336_systems.flash_attention import FlashAttention2
 from typing import Callable
 
-# problem size
-n_heads = 16
-d_head = 64
-#sequence_length = 16384
-sequence_length = 128
 
-# q, k, v with grads, on GPU, BF16
-q, k, v = torch.randn(
-    3, n_heads, sequence_length, d_head,
-    device='cuda', dtype=torch.bfloat16, requires_grad=True
-).unbind(0)
+def main():
 
-# compile the custom autograd Function's .apply
-flash = torch.compile(FlashAttention2.apply)
+    # problem size
+    n_heads = 16
+    d_head = 64
+    #sequence_length = 16384
+    sequence_length = 128
+
+    # q, k, v with grads, on GPU, BF16
+    q, k, v = torch.randn(
+        3, n_heads, sequence_length, d_head,
+        device='cuda', dtype=torch.bfloat16, requires_grad=True
+    ).unbind(0)
+
+    # compile the custom autograd Function's .apply
+    flash = torch.compile(FlashAttention2.apply)
 
 
-def flash_forward_backward():
-    o = flash(q, k, v, True)  # causal=True
-    loss = o.sum() # type: ignore[attr-defined]
-    loss.backward()
+    def flash_forward_backward():
+        o = flash(q, k, v, True)  # causal=True
+        loss = o.sum() # type: ignore[attr-defined]
+        loss.backward()
 
-results = triton.testing.do_bench(flash_forward_backward, warmup=100, rep=500)
-print(results)
+    results = triton.testing.do_bench(flash_forward_backward, warmup=100, rep=500)
+    print(results)
+
+
+if __name__ == '__main__':
+    main()
